3b45aff0be3851d41723fa8187fe55dda5df4efb
martinmeerAT <olegmms@gmail.com3>
2025-01-24 16:29:36 +0300

    upd

--- js/common/menu.js
+++ js/common/menu.js
@@ -1,7 +1,15 @@
-export function loadMenu() {
-  fetch('menu.html')
-    .then(response => response.text())
-    .then(data => {
-      document.querySelector('aside.menu').innerHTML = data;
-    });
-}
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../html/menu.html') // Убедитесь, что путь к menu.html правильный
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка загрузки меню: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.querySelector('aside.menu').innerHTML = data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
