bc6a134ab7fe819083e33566e08ee8254263e6fa
oleg <olegmms@gmail.com>
2025-02-19 01:53:59 +0300

    fix() base url

--- js/common/config.js
+++ js/common/config.js
@@ -1,6 +1,6 @@
 // config.js
 const AppConfig = {
-  API_BASE_URL: '192.144.12.110:8080' // Базовый URL сервера
+  API_BASE_URL: 'http://192.144.12.110:8080' // Базовый URL сервера
 };
 
 export default AppConfig;
