8edd6f792c7a79897b5b146038f3ca7791e98db1
oleg <olegmms@gmail.com>
2025-02-20 00:12:35 +0300

    feat() set localhost options

--- js/common/config.js
+++ js/common/config.js
@@ -1,6 +1,7 @@
 // config.js
 const AppConfig = {
-  API_BASE_URL: 'http://192.144.12.110:8080' // Базовый URL сервера
+  //API_BASE_URL: 'http://192.144.12.110:8080' // Базовый URL сервера
+  API_BASE_URL: 'http://localhost:8080'
 };
 
 export default AppConfig;
