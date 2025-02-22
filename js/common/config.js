83520e819cc2681ce3b44683e5ee10c8571c400a
martinmeerAT <olegmms@gmail.com3>
2025-02-22 12:55:30 +0300

    fix() merging

--- js/common/config.js
+++ js/common/config.js
@@ -1,6 +1,11 @@
 // config.js
 const AppConfig = {
+<<<<<<< HEAD
   API_BASE_URL: 'https://api.otk-help.martinmeer.com' // Базовый URL сервера
+=======
+  //API_BASE_URL: 'http://192.144.12.110:8080' // Базовый URL сервера
+  API_BASE_URL: 'http://localhost:8080'
+>>>>>>> origin/master
 };
 
 export default AppConfig;
