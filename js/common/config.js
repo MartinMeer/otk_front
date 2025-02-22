17284e5c9c07f1f5c742890be20ad71516fe0113
martinmeerAT <olegmms@gmail.com3>
2025-02-22 13:11:20 +0300

    fix() endpoint

--- js/common/config.js
+++ js/common/config.js
@@ -1,11 +1,10 @@
 // config.js
 const AppConfig = {
-<<<<<<< HEAD
+
   API_BASE_URL: 'https://api.otk-help.martinmeer.com' // Базовый URL сервера
-=======
-  //API_BASE_URL: 'http://192.144.12.110:8080' // Базовый URL сервера
-  API_BASE_URL: 'http://localhost:8080'
->>>>>>> origin/master
+
+  //API_BASE_URL: 'http://localhost:8080'
+
 };
 
 export default AppConfig;
